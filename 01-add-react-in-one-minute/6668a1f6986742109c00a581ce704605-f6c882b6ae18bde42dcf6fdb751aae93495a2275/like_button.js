d71f4458a15638e18274a8bee327939f97e61569
plamen1982 <plamen.hristov@ymail.com>
2022-01-10 14:09:59 +0200

    created simple example in react

--- 01-add-react-in-one-minute/6668a1f6986742109c00a581ce704605-f6c882b6ae18bde42dcf6fdb751aae93495a2275/like_button.js
+++ 01-add-react-in-one-minute/6668a1f6986742109c00a581ce704605-f6c882b6ae18bde42dcf6fdb751aae93495a2275/like_button.js
@@ -1,25 +1,24 @@
 'use strict';
 
-const e = React.createElement;
+const reactElement = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+const LikeButton = () => {
+  const [liked, setLiked] = React.useState(false);
+    if(liked) {
+      return ('You like that. Dont you?')
     }
-
-    return e(
+    return reactElement(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
+      { onClick: () => setLiked(true) },
       'Like'
-    );
-  }
+    )
 }
 
+
+//Here we select/pick/peek with normal document API the #like_button_container from the index.html page
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+//Here we render with the ReactDOM API and our earlier reactElement crated with the React API
+//inside the render function we passing as a first argument our reactElement(which is actually a 
+//function that can take as argument custom React Function Element) and as a second argument we passing
+// the selected normal DOM element from the index.html page
+ReactDOM.render(reactElement(LikeButton), domContainer);
